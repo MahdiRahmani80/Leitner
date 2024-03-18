6220328ed0737dbe472bf7af18014e406aa1c205
Mahdi Rahmani <rahmanimahdi368@gmail.com>
2024-03-18 15:19:52 +0330

    Add Room library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.devtools.ksp")
+    // Timber
     id("io.sentry.android.gradle") version "4.3.1"
 }
 
@@ -35,10 +37,13 @@ android {
         jvmTarget = "1.8"
     }
 
+
     buildFeatures {
         viewBinding = true
     }
+
 }
+
 
 dependencies {
 
@@ -52,5 +57,19 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
+
+    // Room
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    // To use Kotlin Symbol Processing (KSP)
+    ksp("androidx.room:room-compiler:$room_version")
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+    // optional - RxJava3 support for Room
+    implementation("androidx.room:room-rxjava3:$room_version")
+    // optional - Test helpers
+    testImplementation("androidx.room:room-testing:$room_version")
 
 }
